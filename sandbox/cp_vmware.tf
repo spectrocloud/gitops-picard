ded0234780a80fd872d07da66186e321b077f1bf
Saad Malik <saad@spectrocloud.com>
2021-01-17 19:56:55 -0800

    Rename to sandbox

--- sandbox/cp_vmware.tf
+++ sandbox/cp_vmware.tf
@@ -4,60 +4,33 @@
 #   name = var.cluster_cluster_profile_name
 # }
 
-# # Example of a Basic add-on profile
-# resource "spectrocloud_cluster_profile" "cp-addon-vsphere" {
-#   name        = "cp-basic"
-#   description = "basic cp"
-#   cloud       = "vsphere"
-#   type        = "add-on"
-#   pack {
-#     name = "spectro-byo-manifest"
-#     tag  = "1.0.x"
-#     uid  = "5faad584f244cfe0b98cf489"
-#     # layer  = ""
-#     values = <<-EOT
-#       manifests:
-#         byo-manifest:
-#           contents: |
-#             # Add manifests here
-#             apiVersion: v1
-#             kind: Namespace
-#             metadata:
-#               labels:
-#                 app: wordpress
-#                 app3: wordpress3
-#               name: wordpress
-#     EOT
-#   }
-# }
 
-
-data "spectrocloud_pack" "byom" {
+data "spectrocloud_pack" "byom-vsphere" {
   name = "spectro-byo-manifest"
   # version  = "1.0.x"
 }
 
-data "spectrocloud_pack" "csi" {
+data "spectrocloud_pack" "csi-vsphere" {
   name = "csi-vsphere-volume"
   # version  = "1.0.x"
 }
 
-data "spectrocloud_pack" "cni" {
+data "spectrocloud_pack" "cni-vsphere" {
   name    = "cni-calico"
   version = "3.16.0"
 }
 
-data "spectrocloud_pack" "k8s" {
+data "spectrocloud_pack" "k8s-vsphere" {
   name    = "kubernetes"
   version = "1.18.14"
 }
 
-data "spectrocloud_pack" "ubuntu" {
+data "spectrocloud_pack" "ubuntu-vsphere" {
   name = "ubuntu-vsphere"
   # version  = "1.0.x"
 }
 
-resource "spectrocloud_cluster_profile" "profile" {
+resource "spectrocloud_cluster_profile" "vsphere1" {
   name        = "vsphere-picard-3"
   description = "basic cp"
   cloud       = "vsphere"
@@ -81,6 +54,42 @@ resource "spectrocloud_cluster_profile" "profile" {
               name: wordpress
     EOT
   }
+
+  pack {
+    name   = "csi-vsphere-volume"
+    tag    = "1.0.x"
+    uid    = data.spectrocloud_pack.csi.id
+    values = data.spectrocloud_pack.csi.values
+  }
+
+  pack {
+    name   = "cni-calico"
+    tag    = "3.16.x"
+    uid    = data.spectrocloud_pack.cni.id
+    values = data.spectrocloud_pack.cni.values
+  }
+
+  pack {
+    name   = "kubernetes"
+    tag    = "1.18.x"
+    uid    = data.spectrocloud_pack.k8s.id
+    values = data.spectrocloud_pack.k8s.values
+  }
+
+  pack {
+    name   = "ubuntu-vsphere"
+    tag    = "LTS__18.4.x"
+    uid    = data.spectrocloud_pack.ubuntu.id
+    values = data.spectrocloud_pack.ubuntu.values
+  }
+
+}
+
+resource "spectrocloud_cluster_profile" "prodvmware" {
+  name        = "ProdVMware"
+  description = "basic cp"
+  cloud       = "vsphere"
+  type        = "cluster"
 
   pack {
     name   = "csi-vsphere-volume"
