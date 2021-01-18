a02ffc4ced3e5f3a41b5d28a8ee0aad2f1dc5d62
Saad Malik <saad@spectrocloud.com>
2021-01-18 13:27:56 -0800

    Ok try2

--- sandbox/cp_vmware.tf
+++ sandbox/cp_vmware.tf
@@ -5,9 +5,19 @@
 # }
 
 
-data "spectrocloud_pack" "byom-vsphere" {
-  name = "spectro-byo-manifest"
-  # version  = "1.0.x"
+data "spectrocloud_pack" "hipster-vsphere" {
+  name = "sapp-hipster"
+  version  = "2.0.0"
+}
+
+data "spectrocloud_pack" "lbmetal-vsphere" {
+  name = "lb-metallb"
+  version  = "0.8.3"
+}
+
+data "spectrocloud_pack" "istio-vsphere" {
+  name = "istio"
+  version  = "1.6.2"
 }
 
 data "spectrocloud_pack" "csi-vsphere" {
@@ -30,66 +40,71 @@ data "spectrocloud_pack" "ubuntu-vsphere" {
   # version  = "1.0.x"
 }
 
-resource "spectrocloud_cluster_profile" "vsphere1" {
-  name        = "vsphere-picard-3"
-  description = "basic cp"
-  cloud       = "vsphere"
-  type        = "cluster"
-
-  pack {
-    name   = "spectro-byo-manifest"
-    tag    = "1.0.x"
-    uid    = data.spectrocloud_pack.byom-vsphere.id
-    values = <<-EOT
-      manifests:
-        byo-manifest:
-          contents: |
-            # Add manifests here
-            apiVersion: v1
-            kind: Namespace
-            metadata:
-              labels:
-                app: wordpress
-                app3: wordpress9
-              name: wordpress
-    EOT
-  }
-
-  pack {
-    name   = "csi-vsphere-volume"
-    tag    = "1.0.x"
-    uid    = data.spectrocloud_pack.csi-vsphere.id
-    values = data.spectrocloud_pack.csi-vsphere.values
-  }
-
-  pack {
-    name   = "cni-calico"
-    tag    = "3.16.x"
-    uid    = data.spectrocloud_pack.cni-vsphere.id
-    values = data.spectrocloud_pack.cni-vsphere.values
-  }
-
-  pack {
-    name   = "kubernetes"
-    tag    = "1.18.x"
-    uid    = data.spectrocloud_pack.k8s-vsphere.id
-    values = data.spectrocloud_pack.k8s-vsphere.values
-  }
-
-  pack {
-    name   = "ubuntu-vsphere"
-    tag    = "LTS__18.4.x"
-    uid    = data.spectrocloud_pack.ubuntu-vsphere.id
-    values = data.spectrocloud_pack.ubuntu-vsphere.values
-  }
-
-}
-
 resource "spectrocloud_cluster_profile" "prodvmware" {
   name        = "ProdVMware"
   description = "basic cp"
   cloud       = "vsphere"
   type        = "cluster"
+
+  pack {
+    name   = "lb-metallb"
+    tag    = "0.8.x"
+    uid    = data.spectrocloud_pack.lbmetal-vsphere.id
+    values = <<-EOT
+      manifests:
+        metallb:
+
+          #The namespace to use for deploying MetalLB
+          namespace: "metallb-system"
+
+          #MetalLB will skip setting .0 & .255 IP address when this flag is enabled
+          avoidBuggyIps: true
+
+          # Layer 2 config; The IP address range MetalLB should use while assigning IP's for svc type LoadBalancer
+          # For the supported formats, check https://metallb.universe.tf/configuration/#layer-2-configuration
+          addresses:
+          - 10.10.182.0-10.10.182.9
+    EOT
+  }
+
+  pack {
+    name   = "sapp-hipster"
+    tag    = "2.0.x"
+    uid    = data.spectrocloud_pack.hipster-vsphere.id
+    values = data.spectrocloud_pack.hipster-vsphere.values
+  }
+
+  pack {
+    name   = "istio"
+    tag    = "1.6.x"
+    uid    = data.spectrocloud_pack.istio-vsphere.id
+    values = <<-EOT
+      charts:
+        istio-operator:
+          hub: docker.io/istio
+          tag: 1.6.2
+          operatorNamespace: istio-operator
+          istioNamespace: istio-system
+        istio-controlplane:
+          namespace: istio-system
+          controlPlaneName: istio-controlplane
+          ############################################ ISTIO PROFILES #################################################
+          # default: generally for production (istiod, prometheus, ingress gateway)
+          # demo: enable everything for production (istiod, prometheus, ingress gateway, egress, kiali, tracing)
+          # remote: remote cluster of a multicluster mesh, with a shared control plane
+          ############################################################################################################
+          spec:
+            profile: demo
+            # Disable Pod disruption budget for the additional components. Otherwise, pods will get stuck when node is drained
+            values:
+              kiali:
+                service:
+                  type: LoadBalancer
+              global:
+                defaultPodDisruptionBudget:
+                  enabled: false
+    EOT
+  }
 
   pack {
     name   = "csi-vsphere-volume"
