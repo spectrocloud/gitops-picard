d221618aa22c0f318b69e0da49e9fc44bac7a385
Saad Malik <saad@spectrocloud.com>
2021-01-27 15:07:16 -0800

    hello123

--- discovery/cp_vmware.tf
+++ discovery/cp_vmware.tf
@@ -37,7 +37,7 @@ data "spectrocloud_pack" "cni-vsphere" {
 
 data "spectrocloud_pack" "k8s-vsphere" {
   name    = "kubernetes"
-  version = "1.18.15"
+  version = "1.18.13"
 }
 
 data "spectrocloud_pack" "ubuntu-vsphere" {
@@ -45,32 +45,32 @@ data "spectrocloud_pack" "ubuntu-vsphere" {
   # version  = "1.0.x"
 }
 
-resource "spectrocloud_cluster_profile" "foo" {
-  name        = "Foo3VMware"
+resource "spectrocloud_cluster_profile" "prodvmware" {
+  name        = "ProdVMware"
   description = "basic cp"
   cloud       = "vsphere"
   type        = "cluster"
 
-  #pack {
-  #  name   = "lb-metallb"
-  #  tag    = "0.8.x"
-  #  uid    = data.spectrocloud_pack.lbmetal-vsphere.id
-  #  values = <<-EOT
-  #    manifests:
-  #      metallb:
+  pack {
+    name   = "lb-metallb"
+    tag    = "0.8.x"
+    uid    = data.spectrocloud_pack.lbmetal-vsphere.id
+    values = <<-EOT
+      manifests:
+        metallb:
 
-  #        #The namespace to use for deploying MetalLB
-  #        namespace: "metallb-system"
+          #The namespace to use for deploying MetalLB
+          namespace: "metallb-system"
 
-  #        #MetalLB will skip setting .0 & .255 IP address when this flag is enabled
-  #        avoidBuggyIps: true
+          #MetalLB will skip setting .0 & .255 IP address when this flag is enabled
+          avoidBuggyIps: true
 
-  #        # Layer 2 config; The IP address range MetalLB should use while assigning IP's for svc type LoadBalancer
-  #        # For the supported formats, check https://metallb.universe.tf/configuration/#layer-2-configuration
-  #        addresses:
-  #        - 10.10.182.0-10.10.182.9
-  #  EOT
-  #}
+          # Layer 2 config; The IP address range MetalLB should use while assigning IP's for svc type LoadBalancer
+          # For the supported formats, check https://metallb.universe.tf/configuration/#layer-2-configuration
+          addresses:
+          - 10.10.182.0-10.10.182.9
+    EOT
+  }
 
   pack {
     name   = "sapp-hipster"
@@ -108,6 +108,70 @@ resource "spectrocloud_cluster_profile" "foo" {
               global:
                 defaultPodDisruptionBudget:
                   enabled: false
+    EOT
+  }
+
+  pack {
+    name   = "csi-vsphere-volume"
+    tag    = "1.0.x"
+    uid    = data.spectrocloud_pack.csi-vsphere.id
+    values = data.spectrocloud_pack.csi-vsphere.values
+  }
+
+  pack {
+    name   = "cni-calico"
+    tag    = "3.16.x"
+    uid    = data.spectrocloud_pack.cni-vsphere.id
+    values = data.spectrocloud_pack.cni-vsphere.values
+  }
+
+  pack {
+    name   = "kubernetes"
+    tag    = "1.18.x"
+    uid    = data.spectrocloud_pack.k8s-vsphere.id
+    values = data.spectrocloud_pack.k8s-vsphere.values
+  }
+
+  pack {
+    name   = "ubuntu-vsphere"
+    tag    = "LTS__18.4.x"
+    uid    = data.spectrocloud_pack.ubuntu-vsphere.id
+    values = data.spectrocloud_pack.ubuntu-vsphere.values
+  }
+
+}
+
+resource "spectrocloud_cluster_profile" "devvmware" {
+  name        = "DevVMware"
+  description = "basic cp"
+  cloud       = "vsphere"
+  type        = "cluster"
+
+  pack {
+    name   = "nginx"
+    tag    = "0.26.x"
+    uid    = data.spectrocloud_pack.nginx-vsphere.id
+    values = data.spectrocloud_pack.nginx-vsphere.values
+  }
+
+  pack {
+    name   = "lb-metallb"
+    tag    = "0.8.x"
+    uid    = data.spectrocloud_pack.lbmetal-vsphere.id
+    values = <<-EOT
+      manifests:
+        metallb:
+
+          #The namespace to use for deploying MetalLB
+          namespace: "metallb-system"
+
+          #MetalLB will skip setting .0 & .255 IP address when this flag is enabled
+          avoidBuggyIps: true
+
+          # Layer 2 config; The IP address range MetalLB should use while assigning IP's for svc type LoadBalancer
+          # For the supported formats, check https://metallb.universe.tf/configuration/#layer-2-configuration
+          addresses:
+          - 10.10.182.0-10.10.182.9
     EOT
   }
 
