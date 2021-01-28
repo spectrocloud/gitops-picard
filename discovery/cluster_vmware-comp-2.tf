84321393b9220362e0b510a75c8f5130c1702665
Saad Malik <saad@spectrocloud.com>
2021-01-27 20:53:04 -0800

    more fun

--- discovery/cluster_vmware-comp-2.tf
+++ discovery/cluster_vmware-comp-2.tf
@@ -1,7 +1,10 @@
 locals {
-  vmware_static_pool_id = "6011e1061d2aa08171553081"
-  datacenter            = "Datacenter"
-  folder                = "Demo"
+  vmware_static_pool_id_1 = "601229dfc6fdece29d6dc124"
+  vmware_static_pool_id_2 = "601229fa8102177f12cf8dda"
+  vmware_static_pool_id_3 = "60122a31c6fdece2a1fe826a"
+
+  datacenter = "Datacenter"
+  folder     = "Demo"
 
   #cluster_network_search = "spectrocloud.local"
 
@@ -14,115 +17,8 @@ data "spectrocloud_cloudaccount_vsphere" "this" {
   name = "demo"
 }
 
-#resource "spectrocloud_cluster_vsphere" "comp-2" {
-#  name               = "vmware-discovery-1"
-#  cluster_profile_id = spectrocloud_cluster_profile.devvmware.id
-#  cloud_account_id   = data.spectrocloud_cloudaccount_vsphere.this.id
-
-#  cloud_config {
-#    ssh_key = local.cluster_ssh_public_key
-
-#    datacenter = local.datacenter
-#    folder     = "Demo/spc-vmware-discovery-1"
-
-#    static_ip = true
-
-#    #network_type          = "VIP"
-#    #network_search_domain = local.cluster_network_search
-#  }
-
-#  # To override or specify values for a cluster:
-
-#  #pack {
-#  #  name   = "lb-metallb"
-#  #  tag    = "0.8.x"
-#  #  values = <<-EOT
-#  #    manifests:
-#  #      metallb:
-
-#  #        #The namespace to use for deploying MetalLB
-#  #        namespace: "metallb-system"
-
-#  #        #MetalLB will skip setting .0 & .255 IP address when this flag is enabled
-#  #        avoidBuggyIps: true
-
-#  #        # Layer 2 config; The IP address range MetalLB should use while assigning IP's for svc type LoadBalancer
-#  #        # For the supported formats, check https://metallb.universe.tf/configuration/#layer-2-configuration
-#  #        addresses:
-#  #        - 10.10.182.100-10.10.182.109
-#  #  EOT
-#  #}
-
-
-#  machine_pool {
-#    control_plane           = true
-#    control_plane_as_worker = true
-#    name                    = "master-pool"
-#    count                   = 3
-
-#    placement {
-#      cluster           = "cluster1"
-#      resource_pool     = ""
-#      datastore         = "datastore54"
-#      network           = "VM Network 2"
-#      static_ip_pool_id = local.vmware_static_pool_id
-#    }
-#    placement {
-#      cluster           = "cluster2"
-#      resource_pool     = ""
-#      datastore         = "datastore55"
-#      network           = "VM Network 2"
-#      static_ip_pool_id = local.vmware_static_pool_id
-#    }
-#    placement {
-#      cluster           = "cluster3"
-#      resource_pool     = ""
-#      datastore         = "datastore56"
-#      network           = "VM Network 2"
-#      static_ip_pool_id = local.vmware_static_pool_id
-#    }
-#    instance_type {
-#      disk_size_gb = 61
-#      memory_mb    = 4096
-#      cpu          = 2
-#    }
-#  }
-
-#  machine_pool {
-#    name  = "worker-pool"
-#    count = 3
-
-#    placement {
-#      cluster           = "cluster1"
-#      resource_pool     = ""
-#      datastore         = "datastore54"
-#      network           = "VM Network 2"
-#      static_ip_pool_id = local.vmware_static_pool_id
-#    }
-#    placement {
-#      cluster           = "cluster2"
-#      resource_pool     = ""
-#      datastore         = "datastore55"
-#      network           = "VM Network 2"
-#      static_ip_pool_id = local.vmware_static_pool_id
-#    }
-#    placement {
-#      cluster           = "cluster3"
-#      resource_pool     = ""
-#      datastore         = "datastore56"
-#      network           = "VM Network 2"
-#      static_ip_pool_id = local.vmware_static_pool_id
-#    }
-#    instance_type {
-#      disk_size_gb = 62
-#      memory_mb    = 4096
-#      cpu          = 2
-#    }
-#  }
-#}
-
-resource "spectrocloud_cluster_vsphere" "comp-3" {
-  name               = "vmware-discovery-2"
+resource "spectrocloud_cluster_vsphere" "discover-1" {
+  name               = "vmware-discovery-1"
   cluster_profile_id = spectrocloud_cluster_profile.devvmware.id
   cloud_account_id   = data.spectrocloud_cloudaccount_vsphere.this.id
 
@@ -172,21 +68,21 @@ resource "spectrocloud_cluster_vsphere" "comp-3" {
       resource_pool     = ""
       datastore         = "datastore54"
       network           = "VM Network 2"
-      static_ip_pool_id = local.vmware_static_pool_id
+      static_ip_pool_id = local.vmware_static_pool_id_1
     }
     placement {
-      cluster           = "cluster22"
+      cluster           = "cluster2"
       resource_pool     = ""
       datastore         = "datastore55"
       network           = "VM Network 2"
-      static_ip_pool_id = local.vmware_static_pool_id
+      static_ip_pool_id = local.vmware_static_pool_id_1
     }
     placement {
-      cluster           = "cluster33"
+      cluster           = "cluster3"
       resource_pool     = ""
       datastore         = "datastore56"
       network           = "VM Network 2"
-      static_ip_pool_id = local.vmware_static_pool_id
+      static_ip_pool_id = local.vmware_static_pool_id_1
     }
     instance_type {
       disk_size_gb = 61
@@ -195,4 +91,35 @@ resource "spectrocloud_cluster_vsphere" "comp-3" {
     }
   }
 
+  machine_pool {
+    name  = "worker-pool"
+    count = 6
+
+    placement {
+      cluster           = "cluster1"
+      resource_pool     = ""
+      datastore         = "datastore54"
+      network           = "VM Network 2"
+      static_ip_pool_id = local.vmware_static_pool_id_1
+    }
+    placement {
+      cluster           = "cluster2"
+      resource_pool     = ""
+      datastore         = "datastore55"
+      network           = "VM Network 2"
+      static_ip_pool_id = local.vmware_static_pool_id_2
+    }
+    placement {
+      cluster           = "cluster3"
+      resource_pool     = ""
+      datastore         = "datastore56"
+      network           = "VM Network 2"
+      static_ip_pool_id = local.vmware_static_pool_id_3
+    }
+    instance_type {
+      disk_size_gb = 62
+      memory_mb    = 4096
+      cpu          = 2
+    }
+  }
 }
