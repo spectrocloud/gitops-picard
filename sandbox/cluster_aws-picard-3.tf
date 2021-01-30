e24f47d7b3449ae34c7adfe109a482cbcc799b2f
Saad Malik <saad@spectrocloud.com>
2021-01-29 21:33:21 -0800

    adding new cluster (#32)
    yahooo

--- sandbox/cluster_aws-picard-3.tf
+++ sandbox/cluster_aws-picard-3.tf
@@ -65,6 +65,65 @@ resource "spectrocloud_cluster_aws" "cluster" {
     instance_type = "t3.large"
     azs           = [local.aws_region_az]
   }
+  
+  machine_pool {
+    name          = "worker-gpu"
+    count         = 1
+    instance_type = "t3.large"
+    azs           = [local.aws_region_az]
+  }
 
 }
+
+
+resource "spectrocloud_cluster_aws" "cluster2" {
+  name               = "aws-picard-5"
+  cluster_profile_id = spectrocloud_cluster_profile.prodaws.id
+  cloud_account_id   = data.spectrocloud_cloudaccount_aws.picard.id
+
+  cloud_config {
+    ssh_key_name = local.aws_ssh_key_name
+    region       = local.aws_region
+  }
+
+  # To override or specify values for a cluster:
+
+  # pack {
+  #   name   = "spectro-byo-manifest"
+  #   tag    = "1.0.x"
+  #   values = <<-EOT
+  #     manifests:
+  #       byo-manifest:
+  #         contents: |
+  #           # Add manifests here
+  #           apiVersion: v1
+  #           kind: Namespace
+  #           metadata:
+  #             labels:
+  #               app: wordpress
+  #               app2: wordpress2
+  #             name: wordpress
+  #   EOT
+  # }
+
+  machine_pool {
+    control_plane           = true
+    control_plane_as_worker = true
+    name                    = "master-pool"
+    count                   = 1
+    instance_type           = "t3.large"
+    disk_size_gb            = 62
+    azs                     = [local.aws_region_az]
+  }
+
+  machine_pool {
+    name          = "worker-basic"
+    count         = 2
+    instance_type = "t3.large"
+    azs           = [local.aws_region_az]
+  }
+
+}
+
+
 
