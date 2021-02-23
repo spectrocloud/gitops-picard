b997455374f385248bb9dbecb72126233a522671
Saad Malik <saad@spectrocloud.com>
2021-02-23 06:54:48 -0800

    Back down
    Back down

--- sandbox/cluster_aws-picard-3.tf
+++ sandbox/cluster_aws-picard-3.tf
@@ -61,7 +61,7 @@ resource "spectrocloud_cluster_aws" "cluster" {
 
   machine_pool {
     name          = "worker-basic"
-    count         = 3
+    count         = 2
     instance_type = "t3.large"
     azs           = [local.aws_region_az]
   }
