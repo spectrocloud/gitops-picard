eb54f88265bfa6b9ad308717538fec8bfbc8734d
Saad Malik <saad@spectrocloud.com>
2021-08-02 11:16:53 -0700

    Change fun

--- sandbox/cluster_vmware-prod-2.tf
+++ sandbox/cluster_vmware-prod-2.tf
@@ -46,7 +46,7 @@ resource "spectrocloud_cluster_vsphere" "prod-vmware-2" {
     placement {
       cluster       = "cluster3"
       resource_pool = ""
-      datastore     = "datastore56_2"
+      datastore     = "Datastore59"
       network       = "VM Network"
     }
     instance_type {
@@ -63,7 +63,7 @@ resource "spectrocloud_cluster_vsphere" "prod-vmware-2" {
     placement {
       cluster       = "cluster3"
       resource_pool = ""
-      datastore     = "datastore56_2"
+      datastore     = "Datastore59"
       network       = "VM Network"
     }
     instance_type {
@@ -80,7 +80,7 @@ resource "spectrocloud_cluster_vsphere" "prod-vmware-2" {
     placement {
       cluster       = "cluster3"
       resource_pool = ""
-      datastore     = "datastore56_2"
+      datastore     = "Datastore59"
       network       = "VM Network"
     }
     instance_type {
