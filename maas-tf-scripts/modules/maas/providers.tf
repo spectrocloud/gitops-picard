43f8272c0b5f2754cd9e18513183ef34340bf1ca
Saad Malik <saad@spectrocloud.com>
2022-12-14 23:58:27 -0800

    ok add maas

---
+++ maas-tf-scripts/modules/maas/providers.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    spectrocloud = {
+      version = ">= 0.10.5"
+      source  = "spectrocloud/spectrocloud"
+    }
+  }
+  experiments      = [module_variable_optional_attrs]
+  required_version = ">= 1.2.8"
+}
