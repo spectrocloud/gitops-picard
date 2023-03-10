ca75c3aa2f388b64717f6346621255f84fe24050
Justin Barksdale <justin@spectrocloud.com>
2023-03-09 19:03:03 -0500

    vault config

--- phoenix/modules/edge/vault.tf
+++ phoenix/modules/edge/vault.tf
@@ -29,24 +29,25 @@ data "local_file" "pem_file" {
   filename = "${path.module}/pem_${local.cluster_id}.pub"
 }
 
-# resource "vault_auth_backend" "kubernetes" {
-#   type = "kubernetes"
-#   # path = "demo"
-# }
+resource "vault_auth_backend" "jwt" {
+  type = "jwt"
+  # path = "demo"
+}
 
-# resource "vault_kubernetes_auth_backend_role" "example" {
-#   backend                          = vault_auth_backend.kubernetes.path
-#   role_name                        = "devweb-app"
-#   bound_service_account_names      = ["internal-app"]
-#   bound_service_account_namespaces = ["default"]
-#   token_ttl                        = 86400
-#   token_policies                   = ["devwebapp"]
-# }
+resource "vault_kubernetes_auth_backend_role" "example" {
+  backend                          = vault_auth_backend.jwt.path
+  role_name                        = "devweb-app"
+  bound_service_account_names      = ["internal-app"]
+  bound_service_account_namespaces = ["default"]
+  token_ttl                        = 86400
+  token_policies                   = ["devwebapp"]
+}
 
-# resource "vault_kubernetes_auth_backend_config" "example" {
-#   backend                = vault_auth_backend.kubernetes.path
-#   kubernetes_host        = local.cluster_host
-#   kubernetes_ca_cert     = base64decode(local.cluster_ca)
-#   token_reviewer_jwt     = kubernetes_secret.vault_sa.data.token
-#   issuer                 = "https://kubernetes.default.svc.cluster.local"
-# }
+resource "vault_kubernetes_auth_backend_config" "example" {
+  backend                = vault_auth_backend.jwt.path
+  # kubernetes_host        = local.cluster_host
+  # kubernetes_ca_cert     = base64decode(local.cluster_ca)
+  # token_reviewer_jwt     = kubernetes_secret.vault_sa.data.token
+  # issuer                 = "https://kubernetes.default.svc.cluster.local"
+  jwt_validation_pubkeys=local_file.pem_file.content
+}
