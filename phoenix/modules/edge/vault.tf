8bb37b70c8c4b40d5600a3dbc939f5346d3934ef
Justin Barksdale <justin@spectrocloud.com>
2023-03-09 20:01:51 -0500

    Updated policies

--- phoenix/modules/edge/vault.tf
+++ phoenix/modules/edge/vault.tf
@@ -39,8 +39,9 @@ resource "vault_jwt_auth_backend_role" "example" {
   backend                          = vault_jwt_auth_backend.example.path
   role_name                        = "devweb-app"
   bound_audiences = ["https://kubernetes.default.svc.cluster.local"]
-
-  user_claim = "sub"
+  bound_subject = "system:serviceaccount:default:default"
+  token_policies = ["devwebapp"]
+  user_claim = "aud"
   role_type       = "jwt"
 
 }
