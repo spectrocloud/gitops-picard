e7eea8794566b6868f8875d5931e697cab7f397a
Boobalan <boobalan@spectrocloud.com>
2023-05-19 16:38:43 -0700

    fix k8s version for profile and clusters

--- discovery/sc-npe-1700.tf
+++ discovery/sc-npe-1700.tf
@@ -26,7 +26,7 @@ module "sc-npe-1700" {
   cluster_profile_id = spectrocloud_cluster_profile.sc-npe-stg.id
   cluster_packs = {
     k8s = {
-      tag  = "1.24.3"
+      tag  = "1.23.9"
       file = "config-stg/k8s.yaml"
     }
     dex = {
