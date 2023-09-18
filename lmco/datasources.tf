0fb540837871d714e045ecbfc5c33760e9a8e137
Boobalan <boobalan@spectrocloud.com>
2023-09-17 23:09:34 -0700

    Updated for addon profile with macros

--- lmco/datasources.tf
+++ lmco/datasources.tf
@@ -10,3 +10,14 @@ data "spectrocloud_cluster_profile" "infra_demo" {
   name = "aws-pxke-infra"
   version = "1.27.2"
 }
+
+# The addon profile to lookup from Palette
+data "spectrocloud_pack" "argo-cd" {
+  name = var.argocd_name
+  version  = var.argocd_version
+  # registry_uid = data.spectrocloud_registry.private-demo.id
+}
+
+#data "spectrocloud_registry" "private-demo" {
+#  name = "Public Repo"
+#}
